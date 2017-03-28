491b6cb7f45a7797ac6aed9dd2ccdfc0fb9e2d81
bogdan <bogdanbudescu96@gmail.com>
2017-03-28 17:13:18 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+//console.log("Hello world");
+var express = require("express");
+
+var app = express();
+
+app.get("/hello",function(req,res){
+	res.send("de");
+});
+
+app.listen(3000,function(){
+	console.log("Server running on 3000");
+})
