8d174d8fbaae0c61edd7d91f4d700fb1e45cd211
bogdan <bogdanbudescu96@gmail.com>
2017-03-28 18:00:20 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 //console.log("Hello world");
 var express = require("express");
-
+var fetch = require("node-fetch");
+var cheerio = require("cheerio");
 var app = express();
 
-app.get("/hello",function(req,res){
-	res.send("de test");
+function processText(text){
+	var $ = cheerio.load(text);
+
+	var listItems = Array.from($("#tiles").children("li"));
+	return listItems.map(function(li){
+		return 	{
+			title: $(li).find(".title").text()
+		};
+	});
+}
+
+app.get("/hello", function(req,res){
+	//res.send("de test");
+	fetch("http://dopopoco.ro/meniu-individual-timisoara")
+	.then(function(response){
+		return response.text();
+	}).then(function(text){
+		res.send(processText(text));
+	});
 });
 
-app.listen(3000,function(){
+app.listen(3000, function(){
 	console.log("Server running on 3000");
 })
+
+//
